9d626eda6721db8ae7798ec8313c5910ce5edd28
tormodhau <tormodhau@gmail.com>
2013-05-15 13:07:17 +0900

    Item class. One object blabla

---
+++ src/item.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tormodhau
+ * Date: 5/15/13
+ * Time: 1:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Item {
+    private final int id;
+    private final List<Integer> categories = new ArrayList<Integer>();
+    private final List<Integer> keywords = new ArrayList<Integer>();
+
+    Item(int id, String categories, String keywords){
+        this.id = id;
+        parseCategories(categories);
+        parseKeywords(keywords);
+    }
+
+
+    private void parseCategories(String categories){
+
+        this.categories.add(123);
+
+
+
+        ///////////
+    }
+
+    private void parseKeywords(String keywords){
+
+
+
+        this.keywords.add(321);
+
+
+        /////////
+
+    }
+
+
+    public List<Integer> getKeywords() {
+        return keywords;
+    }
+
+    public List<Integer> getCategories() {
+        return categories;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
