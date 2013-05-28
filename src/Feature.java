2cba8ca6cf9d4425e0eac3fd8778c75771d4b28b
Lukas Wensby <wensby@gmail.com>
2013-05-28 18:22:57 +0900

    Created the Feature class

---
+++ src/Feature.java
@@ -0,0 +1,14 @@
+/**
+ * Feature class
+ * @author Lukas J. Wensby
+ */
+
+public class Feature {
+	public final int userID;
+	public final int itemID;
+	
+	public Feature(int userID, int itemID) {
+		this.userID = userID;
+		this.itemID = itemID;
+	}
+}
