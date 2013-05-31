9813bc952b6a272e342f05933603d8f234cc3450
tormodhau <tormodhau@gmail.com>
2013-05-31 14:56:08 +0900

    made awesomeness, refactor database to see the result

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,25 @@ public class Main{
 
     public static void main(String []args) throws Exception{
 
-        /*
+
     	Database db = new Database("DB_MAY31");
     	db.openConnection();
         long startTime = System.currentTimeMillis();
 
-        //Database.indexAllTables(db);
-        DataPreparer dp = new DataPreparer(db,10000000);
+        //Database.vacuumDatabase(db);
+        Database.indexAllTables(db);
+        //Database.dropAllTableIndexes(db);
+
+        //DataPreparer dp = new DataPreparer(db,10000000);
 
         long stopTime = System.currentTimeMillis();
         long elapsedTime = stopTime - startTime;
         System.out.println("Time in Ms: " + elapsedTime);
     	db.closeConnection();
         System.out.println("Done!");
-        */
 
-        Tormod_classifier.test_Svm();
+
+        //Tormod_classifier.test_Svm();
 
     }
 }
