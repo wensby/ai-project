87cd64dd7a1ede514096c001a9fa3eee45171e73
Lukas Wensby <wensby@gmail.com>
2013-06-05 14:37:15 +0900

    And after a long time, maybe.. just maybe, is the svm tree working.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-import com.sun.xml.internal.bind.v2.TODO;
+import java.util.LinkedList;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Vector;
 
 /**
  * The Main-class.
@@ -14,42 +11,10 @@ public class Main{
 
 
     public static void main(String []args) throws Exception{
-
-    	Debug.start("test");
-    	Debug.start("test2");
-    	
-    	float a = 100000000;
-    	for (int i = 0; i < a; i++) {
-    		float b = 2;
-    		b = a + b;
-    		Debug.stop("test2");
-    		Debug.start("test2");
-    	}
-    	
-    	Debug.stop("test");
-    	
-    	Debug.pt("test");
-    	Debug.pt("test2");
-    	
-    	/**
-
-    	Database db = new Database("DB_JUN2");
-    	db.openConnection();
-        long startTime = System.currentTimeMillis();
-
-        //DataPreparer dp = new DataPreparer(db,1000);
-
-        SvmInterface.Example.TestSimpleSvm();
-
-
-        long stopTime = System.currentTimeMillis();
-        long elapsedTime = stopTime - startTime;
-        System.out.println("Time in Ms: " + elapsedTime);
-    	db.closeConnection();
-        System.out.println("Done!");
-
-
-        //Tormod_classifier.test_Svm();
-		*/
+    	SvmTree tree = new SvmTree("1110");
+    	tree.addSvm("filepath1", "1000");
+    	tree.addSvm("filepath2", "0100");
+    	LinkedList<String> svms = tree.getSvms("0100");
+    	Debug.pl(svms.getFirst());
     }
 }
